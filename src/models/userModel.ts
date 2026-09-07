46aa32d00127929cb3ac856e11b850678d59069b
auliasabillah <auliasabillah99@gmail.com>
2026-09-07 15:44:12 +0700

    first commit

---
+++ src/models/userModel.ts
@@ -0,0 +1,15 @@
+import pool from '../config/db.js'
+
+export const UserModel = {
+    findByUsername: async (username: string) => {
+        const [rows]: any = await pool.query('SELECT * FROM users WHERE username = ?', [username]);
+        return rows[0];
+    },
+    create: async (username: string, email: string, hashedPssword: string) => {
+        const [result]: any = await pool.query(
+            'INSERT INTO users (username, email, password) VALUES (?, ?, ?)',
+            [username, email, hashedPssword]
+        );
+        return result.insretId;
+    }
+};
